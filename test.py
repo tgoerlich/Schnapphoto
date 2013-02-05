7d6489b08b13479b28c28f1df6fad574d0180c03
tgoerlich <thomas@schnappmatik.de>
2013-02-05 23:20:27 +0100

    tests

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import schnapphoto		
 
 cam = schnapphoto.CameraHost("true")
-#print cam.get_model()
+print cam.get_model()
 print cam.get_capturesetting("f-number")
+print cam.get_capturesetting_all_options("f-number")
+#print "-------------------------"
 #print cam.get_capturesetting_all_options("f-number")
-print "-------------------------"
-#print cam.get_capturesetting_all_options("f-number")
-cam.set_capturesetting("f-number","f/4.2")
-print cam.get_capturesetting("f-number")
+#cam.set_capturesetting("f-number","f/4.5")
 #print cam.get_capturesetting("f-number")
-print cam.pictureloop(count=1,layers=3,evsteps=6,delay=0), "pictures taken"
+#print cam.get_capturesetting("f-number")
+#print cam.pictureloop(loops=1,layers=3,steps=15,bracketingtype="exposurecompensation",delay=0), "pictures taken"
+#print cam.pictureloop(loops=1,layers=3,steps=5,bracketingtype="shutterspeed2",delay=0), "pictures taken"
 #print cam.trigger_picture()
+#print cam.cam.abilities.operations
+print cam.get_abilities("operations")
+cam.close()
