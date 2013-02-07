bbc598f1482b25fbdf542e112ea1d6c800c3fe5d
tgoerlich <thomas@schnappmatik.de>
2013-02-07 23:28:56 +0100

    testing

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import schnapphoto
 
 cam = schnapphoto.CameraHost("true")
 print cam.get_model()
-print cam.get_capturesetting("f-number")
-print cam.get_capturesetting_all_options("f-number")
+#print cam.get_capturesetting("f-number")
+#print cam.get_capturesetting_all_options("f-number")
 #print "-------------------------"
 #print cam.get_capturesetting_all_options("f-number")
 #cam.set_capturesetting("f-number","f/4.5")
@@ -13,5 +13,11 @@ print cam.get_capturesetting_all_options("f-number")
 #print cam.pictureloop(loops=1,layers=3,steps=5,bracketingtype="shutterspeed2",delay=0), "pictures taken"
 #print cam.trigger_picture()
 #print cam.cam.abilities.operations
-print cam.get_abilities("operations")
+#print cam.get_abilities("operations")
+#print cam.get_camerainfo()
+
+print cam.get_file_index()
+
+# this returns "piggyphoto.libgphoto2error: Bad parameters (-2)" with the IXUS attached:
+# print cam.cam.get_config()
 cam.close()
